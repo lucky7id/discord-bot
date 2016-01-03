15969b1f496a3eab4a98d648840cd644f85c6d78
Brandon Thomas <lucky7id@gmail.com>
2016-01-03 00:04:39 -0800

    This time with feeling

--- src/commands.js
+++ src/commands.js
@@ -391,11 +391,11 @@ let startupCmds = [
         description: 'Lists current threads being watched for',
         fn: function (params) {
             let watches = Object.keys(this.threadFinders)
-                .filter((id) => {
+                .filter(id => {
                     let watch = this.threadFinders[id];
                     return watch.channel === params.channelID;
                 })
-                .map((watch id) => {
+                .map(id => {
                     let watch = this.threadFinders[id];
                     return `ID: ${id} /${watch.board}/ - ${watch.keywords.join(' ')}`
             });
